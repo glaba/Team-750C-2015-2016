e938fdc4b8b80a338fdc68ce2a834455e1f53b2d
Siddharth Kurella <siddharthkurella@gmail.com>
2015-09-12 20:27:27 -0400

    Added chassis and transmission code

---
+++ include/motors.h
@@ -0,0 +1,16 @@
+#ifndef MOTORS_H_
+#define MOTORS_H_
+
+#include <API.h>
+
+#define LEFT_MOTOR_TOP 2
+#define LEFT_MOTOR_MID 3
+#define LEFT_MOTOR_BOT 4
+
+#define RIGHT_MOTOR_TOP 5
+#define RIGHT_MOTOR_MID 6
+#define RIGHT_MOTOR_BOT 7
+
+#define TRANSMISSION_SERVO 8
+
+#endif
---
+++ include/sensors.h
@@ -0,0 +1,14 @@
+#ifndef SENSORS_H_
+#define SENSORS_H_
+
+#include <API.h>
+
+#define LEFT_ENC_TOP 1
+#define LEFT_ENC_BOT 2
+extern Encoder leftenc;
+
+#define RIGHT_ENC_TOP 3
+#define RIGHT_ENC_BOT 4
+extern Encoder rightenc;
+
+#endif
